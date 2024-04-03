4ebc08ceec3ed8ce434837125d29e005c3d1f408
Ahmad <abuallaban.ahmad2002@gmail.com>
2024-04-03 23:51:50 +0500

    new project

--- bizzadv/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class Main {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-    }
-}
